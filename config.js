a90965b3406e4dc5f187e23f3c7a4880ff939837
EKO HENDRATNO <eko.hendratno@gmail.com>
2025-02-14 10:58:18 +0700

    v1.09

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
+console.log("Connecting to MySQL with:");
+console.log("Host:", process.env.MYSQLHOST);
+console.log("User:", process.env.MYSQLUSER);
+console.log("Port:", process.env.MYSQLPORT);
+console.log("Database:", process.env.MYSQLDATABASE);
+
 module.exports = {
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
