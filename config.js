5f6bdc42e662e904876e0d7e281977d6a498cc7d
EKO HENDRATNO <eko.hendratno@gmail.com>
2025-02-14 10:36:48 +0700

    v1.08

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ module.exports = {
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
